67903a058c5c9c41d29693ed9f54aedfab04518d
brayangomez22 <gomezmancobrayanalexander@gmail.com>
2021-04-04 12:44:32 -0500

    the state was created with the tasks to be painted as example

--- src/App.js
+++ src/App.js
@@ -1,9 +1,38 @@
-
+import React, { useState } from "react";
 
 function App() {
+
+  const [userName, setUserName] = useState('brayan')
+  const [taskItems, setTaskItems] = useState([
+    {name: 'Task One', done: false},
+    {name: 'Task Two', done: false},
+    {name: 'Task Three', done: true},
+    {name: 'Task Four', done: false},
+  ])
+
+  const taskTableRows = () => {
+    return taskItems.map(task => (
+      <tr key={task.name}>
+        <td>{task.name}</td>
+      </tr>
+    ))
+  }
+
   return (
     <div className="App">
       <h1>Hello World</h1>
+      <table>
+        <thead>
+          <tr>
+            <th>Description</th>
+            <th>Done</th>
+          </tr>
+        </thead>
+
+        <tbody>
+          {taskTableRows()}
+        </tbody>
+      </table>
     </div>
   );
 }
